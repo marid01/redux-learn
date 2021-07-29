defc4424c27f35ddf000a41d14c217fedef10403
Marid <maridshikhkerimov@gmail.com>
2021-07-29 21:07:45 +0300

    #3 Redux JS - connect в react-redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(this.props.testStore);
+    return (
+        <div>
+          <input type="text" />
+          <button>Add track</button>
+          <ul>
+            {this.props.testStore.map((track, index) =>
+                <li key={index}>{track}</li>
+            )}
+          </ul>
+        </div>
+    );
+  }
 }
 
-export default App;
+export default connect(
+    state => ({
+      testStore: state
+    }),
+    dispatch => ({})
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-//
-// ReactDOM.render(
-//     <App />,
-//   document.getElementById('root')
-// );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {Provider} from "react-redux";
+import { createStore } from "redux";
 
 
-import {createStore} from "redux";
+import './index.css';
 
-function playlist (state = [], action) {
+const initialState = [
+    'Smells like spirit',
+    'Enter Sandman'
+];
+
+function playlist(state = initialState, action) {
     if (action.type === 'ADD_TRACK') {
         return [
             ...state,
@@ -22,22 +24,45 @@ function playlist (state = [], action) {
 
 const store = createStore(playlist);
 
-const addTrackBtn = document.querySelectorAll('.addTrack')[0];
-const trackInput = document.querySelectorAll('.trackInput')[0];
-const list = document.querySelectorAll('.list')[0];
 
-store.subscribe(() => {
-    list.innerHTML = '';
-    trackInput.value = '';
-    store.getState().forEach(track => {
-        const li = document.createElement('li');
-        li.textContent = track;
-        list.appendChild(li);
-    });
-})
+ReactDOM.render(
+<Provider store={store}>
+    <App />,
+</Provider>,
+  document.getElementById('root')
+);
 
 
-addTrackBtn.addEventListener('click', () => {
-    const trackName = trackInput.value;
-    store.dispatch({ type: 'ADD_TRACK', payload: trackName });
-});
+// import {createStore} from "redux";
+//
+// function playlist (state = [], action) {
+//     if (action.type === 'ADD_TRACK') {
+//         return [
+//             ...state,
+//             action.payload
+//         ];
+//     }
+//     return state
+// }
+//
+// const store = createStore(playlist);
+//
+// const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+// const trackInput = document.querySelectorAll('.trackInput')[0];
+// const list = document.querySelectorAll('.list')[0];
+//
+// store.subscribe(() => {
+//     list.innerHTML = '';
+//     trackInput.value = '';
+//     store.getState().forEach(track => {
+//         const li = document.createElement('li');
+//         li.textContent = track;
+//         list.appendChild(li);
+//     });
+// })
+//
+//
+// addTrackBtn.addEventListener('click', () => {
+//     const trackName = trackInput.value;
+//     store.dispatch({ type: 'ADD_TRACK', payload: trackName });
+// });
