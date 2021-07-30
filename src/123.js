e0b84222b860e47a7ecf649c8385a7da8de0b048
Marid <maridshikhkerimov@gmail.com>
2021-07-30 08:48:01 +0300

    #4 Redux JS - триггер событий

---
+++ src/123.js
@@ -0,0 +1,33 @@
+import {connect} from 'react-redux';
+
+class App extends Component {
+    addTrack() {
+        console.log('addTrack', this.trackInput.value);
+        this.props.onAddTrack(this.trackInput.value);
+        this.trackInput.value = '';
+    }
+
+    render() {
+        console.log(this.props.testStore);
+        return (
+            <div>
+                <input type="text" ref={(input) => {
+                    this.trackInput = input
+                }}/>
+                <button onClick={this.addTrack.bind(this)}>Add track</button>
+                <ul>
+                    {this.props.testStore.map((track, index) =>
+                        <li key={index}>{track}</li>
+
+
+
+
+                        state => ({
+                        testStore: state
+                    }),
+                        dispatch => ({
+                        onAddTrack: (trackName) => {
+                        dispatch({type: 'ADD_TRACK', payload: trackName});
+                    }
+                    })
+                        )(App);
