04fd925a885dd0360aceea4f5933eeeaa1135d1e
Marid <maridshikhkerimov@gmail.com>
2021-07-30 09:52:18 +0300

    #6 ReduxJS - combineReducer

--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
-import {Provider} from "react-redux";
-import { createStore } from "redux";
-
-
 import './index.css';
+import reducer from './reducers';
 
-const initialState = [
-    'Smells like spirit',
-    'Enter Sandman'
-];
-
-function playlist(state = initialState, action) {
-    if (action.type === 'ADD_TRACK') {
-        return [
-            ...state,
-            action.payload
-        ];
-    }
-    return state
-}
-
-const store = createStore(playlist,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
 <Provider store={store}>
